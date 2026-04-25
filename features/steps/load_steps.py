d0bd1a5aff39124fe85e4716208bfcc0e47cc549
Christian Pesch <pu5ch3r@gmail.com>
2026-04-25 08:56:16 -0400

    first BDD

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -47,6 +47,12 @@ def step_impl(context):
     # load the database with new products
     #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row["name"],
+            "description": row["description"],
+            "price": row["price"],
+            "available": row["available"] in ['true', 'True', '1'],
+            "category": row["category"]
+        }
+        context.resp = requests.post(f"{BASE_URL}", json=payload)
+        assert(context.resp.status_code, HTTP_201_CREATED)
